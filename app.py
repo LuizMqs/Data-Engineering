ae4054d8937008100110749affcf663a3f5facd8
anaDias <karol.gulbarg@gmail.com>
2023-07-05 21:53:25 -0300

    update: app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from dash import Dash, html, dcc, Input, Output, callback
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('suicidios_2010_a_2019_cleaned.csv', dtype={'OCUP': str}, low_memory=False)
+from analysis.suicides_by_year import df_merge_pop_total_suicides_total
+from analysis.suicides_by_gender import df_number_of_suicides_by_gender, df_variation_of_suicides_by_gender_between_2010_and_2019
+from analysis.suicides_by_race import df_number_of_suicides_by_race, df_number_of_suicides_by_race_between_2016_and_2019
+
+df_total_suicides_total = df_merge_pop_total_suicides_total
+df_variation_of_suicides_by_gender = df_variation_of_suicides_by_gender_between_2010_and_2019
+df_number_of_suicides_by_race_between = df_number_of_suicides_by_race_between_2016_and_2019
 
 app = Dash(__name__)
 
@@ -17,11 +24,12 @@ app.layout = html.Div(
         dcc.RadioItems(
             id='btnRadio',
             options=[
-                {'label': 'Por estado', 'value': 'estado'},
-                {'label': 'Por genero', 'value': 'genero'},
-                {'label': 'Por idade', 'value': 'idade'},
+                {'label': 'Ano', 'value': 'ano'},
+                {'label': 'Gênero', 'value': 'genero'},
+                {'label': 'Raça', 'value': 'raca'},
+                {'label': 'Macrorregião', 'value': 'macrorregiao'},
             ],
-            value='estado',
+            value='ano',
             className='buttonsRadio',
             inputStyle={'visibility': 'hidden'},  #oculta o radio button
         ),
@@ -35,33 +43,108 @@ app.layout = html.Div(
     [Input('btnRadio', 'value')]
 )
 def updateGraph(selected_value):
-    if selected_value == 'estado':
-        figure = dcc.Graph(
-                    figure = px.bar(
-                        df.groupby('estado').size().reset_index(name='Quantidade de Mortes'),
-                        x='estado', 
-                        y='Quantidade de Mortes',
-                        title='Quantidade de suicídios por estado',
-                        text='Quantidade de Mortes'
-                    )
-                )
+    if selected_value == 'ano':
+        fig_line = px.line(
+            df_total_suicides_total,
+            x='year',
+            y='suicide_rate',
+            title='Taxa de Suicídio por Ano (2010 - 2019) ',
+            labels={'year': 'Ano', 'suicide_rate': 'Taxa de Suicídio (100.000 habitantes)'}
+        )
+        
+        figures = dcc.Graph(figure=fig_line)
+        
     elif selected_value == 'genero':
-        figure = dcc.Graph(
-                    figure=px.pie(df, 
-                        names='RACACOR', 
-                        title='Distribuição das raças na ocorrência de suicídio')
-                )
-    else:
-        figure = dcc.Graph(
-                    figure = px.bar(
-                        df,
-                        x=df.groupby('ano')['age'].mean().index,
-                        y=df.groupby('ano')['age'].mean()
-                    )
-                )
-    
-    return figure
+        fig_line = px.pie(
+            df_number_of_suicides_by_gender,
+            values='amount_of_ suicides',
+            names='gender',
+            title='Taxa de suicídio por gênero'
+        )
+        
+        fig_scatter = go.Figure()
+        
+        fig_scatter.add_trace(go.Scatter(
+            x=df_variation_of_suicides_by_gender['year'],
+            y=df_variation_of_suicides_by_gender['Variation_Male'],
+            name="Masculino"
+        ))
+        
+        fig_scatter.add_trace(go.Scatter(
+            x=df_variation_of_suicides_by_gender['year'],
+            y=df_variation_of_suicides_by_gender['Variation_Female'],
+            name="Feminino"
+        ))
+        
+        fig_scatter.update_layout(title="Taxa de dispersao entre os generos", xaxis_title="Anos", yaxis_title="Dispersao", legend_orientation='h', legend_valign="bottom",legend_title_text="Legenda", legend_title_side="top", legend = {"yanchor": "bottom", "y": -0.35})
+        
+        figures = [
+            dcc.Graph(figure=fig_scatter),
+            dcc.Graph(figure=fig_line)
+        ]
 
+    elif selected_value == 'raca':
+        
+        fig_pie = px.pie(
+            df_number_of_suicides_by_race,
+            values='amount_of_ suicides',
+            names='race',
+            title='Taxa de suicídio por gênero'
+        )
+        
+        fig_scatter = go.Figure()
+        
+        fig_scatter.add_trace(go.Scatter(
+            x=df_number_of_suicides_by_race_between['year'],
+            y=df_number_of_suicides_by_race_between['Amarela'],
+            name='Amarela'
+        ))
+        
+        fig_scatter.add_trace(go.Scatter(
+            x=df_number_of_suicides_by_race_between['year'],
+            y=df_number_of_suicides_by_race_between['Branca'],
+            name='Branca'
+        ))
+        
+        fig_scatter.add_trace(go.Scatter(
+            x=df_number_of_suicides_by_race_between['year'],
+            y=df_number_of_suicides_by_race_between['Parda'],
+            name='Parda'
+        ))
+        
+        fig_scatter.add_trace(go.Scatter(
+            x=df_number_of_suicides_by_race_between['year'],
+            y=df_number_of_suicides_by_race_between['Indígena'],
+            name='Indígena'
+        ))
+        
+        fig_scatter.add_trace(go.Scatter(
+            x=df_number_of_suicides_by_race_between['year'],
+            y=df_number_of_suicides_by_race_between['Preta'],
+            name='Preta'
+        ))
+        
+        fig_scatter.update_layout(title="Taxa de dispersao entre os generos", xaxis_title="Anos", yaxis_title="Dispersao", legend_orientation='h', legend_valign="bottom",legend_title_text="Legenda", legend_title_side="top", legend = {"yanchor": "bottom", "y": -0.35})
+        
+        figures = [
+            dcc.Graph(figure=fig_scatter),
+            dcc.Graph(figure=fig_pie)
+        ]
+        
+    #elif selected_value == 'macrorregiao':
+        
+        #figures = figure=fig_scatter
+        
+    else: 
+        figures = dcc.Graph(
+            figure = px.bar(
+                df_variation_of_suicides_by_gender,
+                values=['Feminino', 'Masculino'],
+                names=['Feminino', 'Masculino'],
+                title='Distribuição da Taxa de Suicídio por Gênero'
+            )
+        )
+    return figures
 
 #Executando o aplicativo
 if __name__ == '__main__':
